8f860e0a9b8b34686af525f8b2f61907679dd8ad
RuhiMahadeshwar <61055561+imruhi@users.noreply.github.com>
2025-09-22 16:24:13 +0200

    added batched topic transform

--- data/datasets_all/topic_alignment.py
+++ data/datasets_all/topic_alignment.py
@@ -1,11 +1,7 @@
 import os.path
 import pickle
-import pandas as pd
-
 from datasets import load_from_disk
 from bertopic import BERTopic
-from collections import Counter
-import matplotlib.pyplot as plt
 from sentence_transformers import SentenceTransformer
 
 
@@ -23,16 +19,28 @@ def align_text(embed_model_name="sentence-transformers/distiluse-base-multilingu
                                           model_kwargs={"torch_dtype": "float16"})
     loaded_model = BERTopic.load(topic_model_path, embedding_model=embedding_model)
 
-    if os.path.exists(ds_path+'_embeds'):
-        ds = load_from_disk(ds_path+'_embeds')
+    ds = load_from_disk(ds_path)
+
+    if os.path.exists("all_texts_embeddings.pkl"):
+        with open("all_texts_embeddings.pkl", 'rb') as p:
+            embeddings = pickle.load(p)
     else:
-        ds = load_from_disk(ds_path)
         embeddings = embedding_model.encode(ds["CleanedText"], show_progress_bar=True)
         # TODO: add metadata for embedding model name?
-        ds = ds.add_column("Embeddings", embeddings)
-        ds.save_to_disk(ds_path+"_embeds")
+        with open("all_texts_embeddings.pkl", 'wb') as p:
+            pickle.dump(embeddings, p)
 
-    topics, probs = loaded_model.transform(documents=ds['CleanedText'], embeddings=ds["Embeddings"])
-    ds = ds.add_column("Topic", topics)
-    ds.save_to_disk(ds_path+"_embeds_topics")
+    if not os.path.exists(ds_path+"_topics"):
+        ds = ds.remove_columns("__index_level_0__")
 
+        iter = ds.iter(batch_size=10000)
+        topics = []
+        i = 0
+        for subset in iter:
+            ts, probs = loaded_model.transform(documents=subset['CleanedText'],
+                                               embeddings=embeddings[i:i + len(subset["CleanedText"])])
+            topics.extend(list(ts))
+            i += len(subset["CleanedText"])
+
+        ds = ds.add_column("Topic", topics)
+        ds.save_to_disk(ds_path + "_topics")
